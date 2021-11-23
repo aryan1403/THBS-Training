2f2569513a7eebc8422650acb8d2356e9ea0fd2e
Aaryan <aaryan14032006@gmail.com>
2021-11-23 12:33:11 +0530

    Learned Methods

---
+++ Day2/methods.java
@@ -0,0 +1,9 @@
+public class methods {
+    public static void add(int a, int b) {
+        System.out.println("Sum : " + (a + b));
+    }
+    public static void main(String[] args) {
+        add(2, 3);
+        add(10, -8);
+    }
+}
