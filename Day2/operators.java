c7c4ee7ed0cc3954d7a8fc8aae28b87b0bf272a7
Aaryan <aaryan14032006@gmail.com>
2021-11-23 12:33:24 +0530

    Learned Operators in Java

---
+++ Day2/operators.java
@@ -0,0 +1,17 @@
+public class operators {
+    public static void main(String[] args) {
+        System.out.println(3 >= 3 && 4 < 2);
+
+        /* && (AND)
+         * This operator checks whether both of the conditions are true.
+         * if any of them is false then it will return false else true. 
+        */
+
+        System.out.println(3 < 3 || 4 < 2);
+
+        /* || (OR)
+         * This operator checks whether any of the conditions are true.
+         * if any of them is true then it will return true else false. 
+        */
+    }
+}
