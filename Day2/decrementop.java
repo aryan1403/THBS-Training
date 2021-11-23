52708e32bc17aefcf73c30b6d491e198051d0748
Aaryan <aaryan14032006@gmail.com>
2021-11-23 12:32:24 +0530

    Learned Decrement operator

---
+++ Day2/decrementop.java
@@ -0,0 +1,10 @@
+public class decrementop {
+    public static void main(String[] args) {
+        int a = 15;
+
+        a--; // postfix
+        --a; //prefix
+        System.out.println(a);
+        System.out.println(--a);
+    }
+}
