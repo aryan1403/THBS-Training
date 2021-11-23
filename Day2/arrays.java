7c0efa25d88e68cc006bfff037b69877018bfa67
Aaryan <aaryan14032006@gmail.com>
2021-11-23 15:15:03 +0530

    Learned Arrays

---
+++ Day2/arrays.java
@@ -0,0 +1,13 @@
+public class arrays {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        arr[0] = -1;
+        arr[1] = 7;
+        arr[2] = 13;
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
