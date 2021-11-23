3ea3effcc330fab2d9d14bf48f3bcb9a0b0ee711
Aaryan <aaryan14032006@gmail.com>
2021-11-23 12:32:55 +0530

    Learned increment operator

---
+++ Day2/incrementop.java
@@ -0,0 +1,8 @@
+public class incrementop {
+    public static void main(String[] args) {
+        int a = 14;
+        a++; // postfix
+        System.out.println(++a); //prefix
+        System.out.println(a);
+    }
+}
