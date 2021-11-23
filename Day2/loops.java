be9c7eb8c9c76ef86262650468f12d0cb9b2b493
Aaryan <aaryan14032006@gmail.com>
2021-11-23 12:33:04 +0530

    Learned Loops

---
+++ Day2/loops.java
@@ -0,0 +1,14 @@
+public class loops {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        while(counter < 5) {
+            // System.out.println(counter);
+            counter++;
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
