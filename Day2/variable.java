cbf559597c6341ab186c826de0d0f34325546e2b
Aaryan <aaryan14032006@gmail.com>
2021-11-23 12:33:46 +0530

    Learned Variables in Java

---
+++ Day2/variable.java
@@ -0,0 +1,33 @@
+public class variable {
+    public static void main(String[] args){
+        // Integer
+        int a = 5;
+        int b = 10;
+
+        // Double
+        double d = 1.5434;
+        System.out.println(d);
+
+        // Character
+        char ch = 'A';
+        System.out.println(ch);
+
+        // String 
+        String s = "Aaryan meow meow...!";
+        System.out.println(s);
+
+        // Boolean
+        boolean bool = true;
+        boolean bool2 = false;
+
+        System.out.println(bool);
+        System.out.println(bool2);
+
+        // Mathematical Operations
+        System.out.println( "Sum is : " + (a + b));
+        System.out.println( "Sub is : " + (a - b));
+        System.out.println( "Product is : " + (a * b));
+        System.out.println( "Division is : " + (a / b));
+        System.out.println( "Remainder is : " + (a % b));
+    }
+}
