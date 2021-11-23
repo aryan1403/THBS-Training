8220e6c9f24028735e6d3a8e2c377d39a371f1d3
Aaryan <aaryan14032006@gmail.com>
2021-11-23 12:33:32 +0530

    ...!xD

---
+++ Day2/ternaryop.java
@@ -0,0 +1,16 @@
+public class ternaryop {
+
+    public static void check(int a, int b) {
+        if(a > b) {
+
+        } else {
+
+        }
+    }
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        check(a, b);
+    }
+}
