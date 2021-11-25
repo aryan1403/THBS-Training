cbf22ac9ad02b0d6fb113cb15dbff84359c52bf9
Aaryan <aaryan14032006@gmail.com>
2021-11-25 11:29:24 +0530

    Updated...

--- Day3/demo/src/main/java/com/example/demo/mycontroller.java
+++ Day3/demo/src/main/java/com/example/demo/mycontroller.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,11 @@ public class mycontroller {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to my site!";
+    }
+
+    @GetMapping("/add/{a}/{b}")
+    public int sum(@PathVariable int a, @PathVariable int b) {
+        return (a+b);
     }
 
     @PostMapping("/addParticipants")
