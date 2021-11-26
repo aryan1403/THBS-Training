b4b733778cc49465a2568bda489b6f67cfbcf24c
Aaryan <aaryan14032006@gmail.com>
2021-11-26 09:44:52 +0530

    Initial commit

---
+++ Day5/shoppingapi/src/main/java/com/example/shoppingapi/ShoppingapiApplication.java
@@ -0,0 +1,11 @@
+package com.example.shoppingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingapiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingapiApplication.class, args);
+	}
+}
---
+++ Day5/shoppingapi/src/test/java/com/example/shoppingapi/ShoppingapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
